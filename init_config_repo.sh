9bf453300c6fc825c48dd6e8b7eab28bbe8dcfa1
Stephen FAURE <faure.stephen@gmail.com>
2019-10-02 10:29:47 +0200

    Update init_config_repo.sh
    Add commands to checkout the latest config files on init

--- init_config_repo.sh
+++ init_config_repo.sh
@@ -5,3 +5,8 @@ alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
 config config --local status.showUntrackedFiles no
 echo "alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'" >> $HOME/.bashrc
 echo "alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'" >> $HOME/.zshrc
+
+# Fetch latest dotfiles
+config remote add origin https://github.com/freohr/dotfiles.git
+config fetch origin
+config reset --hard origin/master
